878e5935000054e6cb77b969ff14815242d10b30
Nils Gajsek <nils.gajsek@glanzkinder.com>
2014-12-04 19:26:49 +0100

    Added changelog file, added default task file structure

---
+++ tasks/jsloc.js
@@ -0,0 +1,38 @@
+/**
+ * jsloc - main object
+ *
+ * @category  javascript-qa-tool
+ * @package   jsloc
+ * @author    Nils Gajsek <info@linslin.org>
+ * @copyright 2014 Nils Gajsek <info@linslin.org>
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version   0.0.1
+ * @link      https://github.com/linslin
+ *
+ */
+
+//set ECMAScript5 strict mode
+'use strict';
+
+
+/**
+ * Module export
+ *
+ * @param grunt
+ */
+module.exports = function(grunt) {
+
+
+    /**
+     * @var {string} path
+     */
+    var path = require('path');
+
+
+    /**
+     * Register multi task `jsloc`
+     */
+    grunt.registerMultiTask('jsloc', 'Yes Sir! Measuring your project. Please hang on... .', function() {
+
+    });
+};
