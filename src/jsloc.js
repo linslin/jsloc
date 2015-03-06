93f8ebc0ad412c64c4f5529cdc18834393a329be
Nils Gajsek <nils.gajsek@glanzkinder.com>
2015-03-06 23:43:11 +0100

    #1 Main structure of "countFiles" added.

---
+++ src/jsloc.js
@@ -0,0 +1,51 @@
+/**
+ * jsloc - main object
+ *
+ * @category  javascript-qa-tool
+ * @package   jsloc
+ * @author    Nils Gajsek <info@linslin.org>
+ * @copyright 2014 Nils Gajsek <info@linslin.org>
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version   0.0.1
+ * @link      https://github.com/linslin
+ *
+ */
+
+
+/**
+ * jsloc object
+ *
+ * @param {object}
+ */
+var jsloc = (function () {
+
+    //set ECMAScript5 strict mode
+    "use strict";
+
+    // ############################################## variable defines // ##############################################
+
+    /**
+     * Holds work directory
+     * @param {string}
+     */
+    var scanDir = '';
+
+    /**
+     * Holds
+     * @param {string}
+     */
+    var options = {
+        'includeHtml': true
+    };
+
+
+    // ############################################## object methods // ##############################################
+
+    /**
+     * Count files of project
+     */
+    function countFiles() {
+
+    }
+
+});
